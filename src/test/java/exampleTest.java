4147d1b1db8992470ac18c4e01e5186c21c5084c
pyavchik <pyavchik@gmail.com>
2018-10-31 12:14:22 +0200

    added ta test

---
+++ src/test/java/exampleTest.java
@@ -0,0 +1,38 @@
+import io.trueautomation.client.driver.TrueAutomationDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.ta;
+
+public class exampleTest {
+    private WebDriver driver;
+    private By loginBtn = By.cssSelector(ta("loginBtn", "a.login-btn"));
+    private By signupBtn = By.cssSelector(ta("signupBtn", "div.sign-up-container > a"));
+    private By emailFl = By.name(ta("emailFl", "email"));
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new TrueAutomationDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void exampleTest() {
+        driver.get("https://trueautomation.io");
+
+        driver.findElement(loginBtn).click();
+        driver.findElement(signupBtn).click();
+
+        driver.findElement(emailFl).sendKeys("your@mail.com");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
